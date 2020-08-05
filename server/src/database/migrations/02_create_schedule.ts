1cb774fa4ef14b72b6d3b2384e112fd4a860e1ab
Matheus Felipe <matheusfelipeog@gmail.com>
2020-08-05 17:53:52 -0300

    Created database and structure of migrations

---
+++ server/src/database/migrations/02_create_schedule.ts
@@ -0,0 +1,22 @@
+import knex from 'knex';
+
+export async function up(knex: knex){
+    return knex.schema.createTable('class_schedule', table => {
+        table.increments('id').primary();
+
+        table.integer('week_day').notNullable();
+        table.integer('from').notNullable();
+        table.integer('to').notNullable();
+
+        table.integer('class_id')
+            .notNullable()
+            .references('id')
+            .inTable('classes')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+    });
+}
+
+export async function down(knex: knex){
+    return knex.schema.dropTable('class_schedule');
+}
