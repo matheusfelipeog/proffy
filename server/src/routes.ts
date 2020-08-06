2ebab7e0ae84a95f2bb75d96a986d4c72f0a62a2
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-08-06 01:21:02 -0300

    Create API route to get classes and data user

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,12 +4,7 @@ import ClassesController from './controllers/ClassesController';
 const routes = express.Router();
 const classesController = new ClassesController();
 
-interface ScheduleItem {
-    week_day: number,
-    from: string,
-    to: string
-}
-
+routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
 export default routes;
