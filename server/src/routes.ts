84497ed96798dfdb06cf75a91195a115df32b67b
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-08-06 02:24:48 -0300

    Create API route to set and get connections

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
