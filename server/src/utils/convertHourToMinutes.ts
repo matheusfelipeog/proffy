fe7fc11b1f177f489c8f3ed7a43a518c3131a858
Matheus Felipe <matheusfelipeog@gmail.com>
2020-08-05 23:25:48 -0300

    Create API route to create new classes

---
+++ server/src/utils/convertHourToMinutes.ts
@@ -0,0 +1,8 @@
+export default function convertHourToMinutes(time: string) {
+
+    const [hour, minutes] = time.split(':').map(Number)
+    const timeInMinutes = (hour * 60) + minutes;
+
+    return timeInMinutes
+    
+}
