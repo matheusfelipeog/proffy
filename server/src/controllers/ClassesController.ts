33faaae2c5e143717691505fb2df7eecbe7817ac
Matheus Felipe <matheusfelipeog@gmail.com>
2020-08-06 01:21:02 -0300

    Create API route to get classes and data user

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -10,6 +10,40 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+
+    async index(request: Request, response: Response){
+        const filters = request.query;
+
+        const week_day = filters.week_day as string; 
+        const subject = filters.subject as string; 
+        const time = filters.time as string; 
+
+        if (!week_day || !subject || !time){
+            return response.status(400).json({
+                error: 'Missing filters to search classes'
+            });
+        }
+
+        const timeInMinutes = convertHourToMinutes(time);
+
+        const classes = await db('classes')
+            .whereExists(function(){
+                this.select('class_schedule.*')
+                    .from('class_schedule')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                    .whereRaw('`class_schedule`.`week_day` = ?', [week_day])
+                    .whereRaw('`class_schedule`.`from` <= ?', [timeInMinutes])
+                    .whereRaw('`class_schedule`.`to` > ?', [timeInMinutes])
+            })
+            .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.*']);
+
+
+        return response.json(classes);
+
+    }
+
     async create(request: Request, response: Response){
 
         const {
