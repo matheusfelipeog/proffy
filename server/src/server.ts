e5b8b49cb4ece008a92bbf01fdbc77117102abd8
Matheus Felipe <matheusfelipeog@gmail.com>
2020-08-06 02:24:48 -0300

    Create API route to set and get connections

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 const port = 3000
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
