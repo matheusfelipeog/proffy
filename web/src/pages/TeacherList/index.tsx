7279638f6447c0575999d696a6d108e14d661307
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-08-06 23:07:01 -0300

    Feat: change two inputs for two new components on the form

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
 
@@ -10,20 +12,40 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
 
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da Semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Geográfia', label: 'Geográfia' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Educação Física', label: 'Educação Física' },
+                            { value: 'Ciências', label: 'Ciências' }
+                        ]}
+                    />
 
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Select
+                        name="week_day"
+                        label="Dia da Semana"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-Feira' },
+                            { value: '2', label: 'Terça-Feira' },
+                            { value: '3', label: 'Quarta-Feira' },
+                            { value: '4', label: 'Quinta-Feira' },
+                            { value: '5', label: 'Sexta-Feira' },
+                            { value: '6', label: 'Sábado' }
+                        ]}
+                    />
+
+                    <Input name="time" label="Hora" type="time" />
+
                 </form>
             </PageHeader>
 
