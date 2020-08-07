099260092d3320730afc5f19f256cacd49feda41
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-08-06 22:46:58 -0300

    Feat: created two new components -> Input and Select

---
+++ web/src/components/Select/index.tsx
@@ -0,0 +1,30 @@
+import React, { SelectHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement>{
+    name: string;
+    label: string;
+    options: Array<{
+        value: string;
+        label: string;
+    }>;
+}
+
+const Select: React.FunctionComponent<SelectProps> = ({ label, name, options, ...rest }) => {
+    return (
+        <div className="select-block">
+            <label htmlFor={name}>{label}</label>
+            <select defaultValue="" id={name} {...rest}>
+
+                <option value="" disabled hidden >Selecione uma opção</option>
+                {options.map(option => {
+                    return <option key={option.value} value={option.value}>{option.label}</option>
+                })}
+
+            </select>
+        </div>
+    );
+}
+
+export default Select;
