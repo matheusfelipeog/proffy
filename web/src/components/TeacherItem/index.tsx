4ea468780b9159af24b3bb7cd90413ff3cd1b48b
Matheus Felipe <matheusfelipeog@gmail.com>
2020-08-07 23:31:21 -0300

    Feat: connect study page with api

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,36 +3,58 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    "id": number,
+    "subject": string,
+    "cost": number,
+    "name": string,
+    "avatar": string,
+    "whatsapp": string,
+    "bio": string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+    
+    function createNewConnection(){
+        api.post('/connections', {
+            user_id: teacher.id
+        });
+    }
+    
     return (
         <article className="teacher-item">
             <header>
                 <img 
-                    src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads02&accessoriesType=Round&hairColor=Platinum&facialHairType=MoustacheMagnum&facialHairColor=Platinum&clotheType=BlazerShirt&eyeType=EyeRoll&eyebrowType=DefaultNatural&mouthType=Concerned&skinColor=Pale"
-                    alt="Uma pessoa"
+                    src={ teacher.avatar }
+                    alt={ teacher.name }
                 />
                 <div>
-                    <strong>Dr. Periódica</strong>
-                    <span>Química</span>
+                    <strong>{ teacher.name }</strong>
+                    <span>{ teacher.subject }</span>
                 </div>
             </header>
 
             <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+            { teacher.bio }
             </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 50,00</strong>
+                    <strong>R$ { teacher.cost }</strong>
                 </p>
-                <button type="button">
+                <a href={ `https://wa.me/${ teacher.whatsapp }` } target="_blank" onClick={ createNewConnection } >
                     <img src={whatsappIcon} alt="Ícone do Whatsapp" />
                     Entrar em Contanto
-                </button>
+                </a>
             </footer>
         </article>
     )
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import Input from '../../components/Input/Input';
 import Select from '../../components/Select';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 function TeacherList() {
+    const [teachers, setTeachers] = useState([]);
+
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    async function searchTeachers(e: FormEvent){
+        e.preventDefault();
+
+        const response = await api.get('/classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        setTeachers(response.data);
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
-                <form id="search-teachers">
+                <form id="search-teachers" onSubmit={ searchTeachers } >
 
                     <Select
                         name="subject"
                         label="Matéria"
+                        value={subject}
+                        onChange={ e => {
+                            setSubject(e.target.value);
+                        }} 
                         options={[
                             { value: 'Artes', label: 'Artes' },
                             { value: 'Biologia', label: 'Biologia' },
@@ -33,6 +59,10 @@ function TeacherList() {
                     <Select
                         name="week_day"
                         label="Dia da Semana"
+                        value={week_day}
+                        onChange={ e => {
+                            setWeekDay(e.target.value);
+                        }}
                         options={[
                             { value: '0', label: 'Domingo' },
                             { value: '1', label: 'Segunda-Feira' },
@@ -44,13 +74,26 @@ function TeacherList() {
                         ]}
                     />
 
-                    <Input name="time" label="Hora" type="time" />
+                    <Input
+                        name="time" 
+                        label="Hora"
+                        type="time"
+                        value={time}
+                        onChange={ e => {
+                            setTime(e.target.value);
+                        }}
+                    />
+                
+                    <button type="submit">Buscar</button>
 
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem />
+                {teachers.map( (teacher: Teacher) => {
+                    return <TeacherItem key={ teacher.id } teacher={ teacher } />
+                })}
+
             </main>
         </div>
     )
